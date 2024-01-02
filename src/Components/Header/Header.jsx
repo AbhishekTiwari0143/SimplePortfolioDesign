333583d1de66aa5b6d6b6bb6992d03b1640e4cfc
AbhishekTiwari0143 <tiwariji2352003@gmail.com>
2024-01-02 10:04:04 +0530

    upload

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
     <header
       className={`${
         scrolling ? "border-b border-gray-600" : ""
-      } md:visible sm: container fixed z-10  pt-4 pb-2 top-0 left-0 right-0 max-w-[70%] w-screen m-auto flex items-center justify-between`}
+      } md:visible sm: container fixed bg-black z-10  pt-4 pb-2 top-0 left-0 right-0 max-w-[70%] w-screen m-auto flex items-center justify-between`}
     >
       <h2 className="text-3xl">Simple Portfolio</h2>
       <ul className="flex gap-4">
